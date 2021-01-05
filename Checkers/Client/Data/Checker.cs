3791526f4806ef2c813f78df1ff127982359d66c
dan <contact@modular.dev>
2021-01-05 15:01:33 -0600

    Add project files.

---
+++ Checkers/Client/Data/Checker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Checkers.Client.Data
+{
+    public class Checker
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public CheckerDirection Direction { get; set; }
+        public string Color { get; set; }
+    }
+
+    public enum CheckerDirection
+	{
+        Down, Up, Both
+	}
+}
